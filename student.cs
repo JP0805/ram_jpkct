92d81ca52f0796440df2e66906c6bde7d8ebf3f5
JP0805 <76272722+JP0805@users.noreply.github.com>
2020-12-21 23:34:45 +0530

    Create student.cs

---
+++ student.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace app
+{
+    public class Student
+    {
+        public  long Id { get; set; }
+        public  string Name { get; set; }
+        public  DateTime age { get; set; }
+        public  string department { get; set; }
+    }
+
+    
